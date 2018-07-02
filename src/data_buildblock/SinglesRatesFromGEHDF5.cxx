537470da46fc6f860ef733f5c16a4d03dd0530db
pw351 <pwadhwa351@gmail.com>
2018-07-02 17:17:51 +0100

    Changes to remove unnecessary outputs

--- src/data_buildblock/SinglesRatesFromGEHDF5.cxx
+++ src/data_buildblock/SinglesRatesFromGEHDF5.cxx
@@ -376,21 +376,17 @@ const int    RANK_OUT = 2;
 
       if( type_class == H5T_INTEGER )
       {
-     std::cout << "Data set has INTEGER type" << std::endl;
-	
+
 //PW Get the integer type
 
      H5::IntType intype = dataset.getIntType();
      
 	H5std_string order_string;
          H5T_order_t order = intype.getOrder( order_string );
-        std::cout << order_string << std::endl;
-
-          //PW Get size of the data element stored in file and print it.
+                  //PW Get size of the data element stored in file and print it.
  
          size_t size = intype.getSize();
-         std::cout << "Data size is " << size << std::endl;
-      }
+              }
 
 //PW Get dataspace of the dataset.   
       H5::DataSpace dataspace = dataset.getSpace();
@@ -402,9 +398,6 @@ const int    RANK_OUT = 2;
       
       hsize_t dims_out[2];
       int ndims = dataspace.getSimpleExtentDims( dims_out, NULL);
-      std::cout << "rank " << rank << ", dimensions " <<
-          (unsigned long)(dims_out[0]) << " x " <<
-          (unsigned long)(dims_out[1]) << std::endl;
 
 //PW Define hyperslab in the dataset; implicitly giving strike and block NULL.
   
